3c28e30f655232fd3c44689000aac76363c8bdde
KarenLivia <55704139+KarenLivia@users.noreply.github.com>
2022-11-04 21:48:18 -0300

    2 files modified, 11 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { HomeComponent } from './components/home/home.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TimerComponent } from './components/timer/timer.component';
+import { RouterModule } from '@angular/router';
+import { TasksService } from './services/tasks.service';
+import { TimerService } from './services/timer.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'tasks', component: TasksComponent },
+      { path: 'timer', component: TimerComponent },
+    ]),
+  ],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    TasksComponent,
+    TimerComponent,
+  ],
+  bootstrap: [AppComponent],
+  providers: [TasksService, TimerService],
 })
-export class AppModule { }
+export class AppModule {}
